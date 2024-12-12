f38c434e3b5bacaf7d4b9432aa4ff5d11d3900b0
Samuel Koch <samuel_koch@sluz.ch>
2024-12-12 14:30:48 +0100

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
 import globals from "globals";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: globals.browser
+  },
+  rules: {
+    "no-unused-vars": "error",
+    "prefer-const": "error",
+    "eqeqeq": "error"
+  }
+};
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import chalk from 'chalk'
 
-console.log(chalk.blue('It works'));
+let myUnusedVariable = 42;
+
+if (myConstantVariable == "Hello World") {
+    console.log(chalk.blue('It works'));
+}
