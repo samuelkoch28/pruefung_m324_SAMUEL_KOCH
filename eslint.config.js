33450dff233d2db0ffde5ec550fb7fd4b23fa664
Samuel Koch <samuel_koch@sluz.ch>
2024-12-12 14:35:57 +0100

    Revert "formatter test"
    This reverts commit 0f8c5baf6c8787401d84bdf6fe1dc8da378bb35a.

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,15 @@
-import globals from 'globals';
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config} */
 export default {
-  languageOptions:
-    {
-      ecmaVersion:
-        'latest',
-      sourceType:
-        'module',
-      globals:
-        globals.browser,
-    },
-  rules: {
-    'no-unused-vars':
-      'error',
-    'prefer-const':
-      'error',
-    eqeqeq:
-      'error',
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: globals.browser
   },
+  rules: {
+    "no-unused-vars": "error",
+    "prefer-const": "error",
+    "eqeqeq": "error"
+  }
 };
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
-if (
-  myConstantVariable ===
-  'Hello World'
-) {
-  console.log(
-    chalk.blue(
-      'It works',
-    ),
-  );
+if (myConstantVariable === "Hello World") {
+    console.log(chalk.blue('It works'));
 }
