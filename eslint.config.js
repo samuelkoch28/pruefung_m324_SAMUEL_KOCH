0f8c5baf6c8787401d84bdf6fe1dc8da378bb35a
Samuel Koch <samuel_koch@sluz.ch>
2024-12-12 14:35:27 +0100

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,22 @@
-import globals from "globals";
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config} */
 export default {
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    globals: globals.browser
-  },
+  languageOptions:
+    {
+      ecmaVersion:
+        'latest',
+      sourceType:
+        'module',
+      globals:
+        globals.browser,
+    },
   rules: {
-    "no-unused-vars": "error",
-    "prefer-const": "error",
-    "eqeqeq": "error"
-  }
+    'no-unused-vars':
+      'error',
+    'prefer-const':
+      'error',
+    eqeqeq:
+      'error',
+  },
 };
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import chalk from 'chalk'
+import chalk from 'chalk';
 
-if (myConstantVariable === "Hello World") {
-    console.log(chalk.blue('It works'));
+if (
+  myConstantVariable ===
+  'Hello World'
+) {
+  console.log(
+    chalk.blue(
+      'It works',
+    ),
+  );
 }
